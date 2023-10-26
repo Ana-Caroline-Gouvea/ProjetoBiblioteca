2955ffc090227b7794b6d3a2e43824ab2ba61139
Ana-Caroline-Gouvea <anagouvea.senai@gmail.com>
2023-10-26 08:27:53 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoBiblioteca.Models
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+    }
+}
