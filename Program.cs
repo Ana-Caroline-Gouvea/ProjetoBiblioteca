e17754fefa295857d2b77759cba0c8554f8dc61d
vitorPr0 <senaivitor899@gmail.com>
2023-10-27 10:09:38 -0300

    home final

--- Program.cs
+++ Program.cs
@@ -17,8 +17,8 @@ builder.Services.AddControllersWithViews();
 /*.Services.AddDbContext<Contexto> //Melina
     (options => options.UseSqlServer("Data Source=SB-1490650\\SQLSENAI;Initial Catalog = ProjetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));*/
 
-/*builder.Services.AddDbContext<Contexto> //Vitor
-    (options => options.UseSqlServer("Data Source=SB-1490654\\SQLSENAI;Initial Catalog = ProjetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));*/
+builder.Services.AddDbContext<Contexto> //Vitor
+    (options => options.UseSqlServer("Data Source=SB-1490654\\SQLSENAI;Initial Catalog = ProjetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));
 
 var app = builder.Build();
 
