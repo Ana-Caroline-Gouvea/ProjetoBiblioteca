fb9cd40d11a5cc1681ef80ca25a6fbf04b01a7a4
vitorPr0 <senaivitor899@gmail.com>
2023-10-27 08:56:02 -0300

    home alterada

--- Program.cs
+++ Program.cs
@@ -12,8 +12,8 @@ builder.Services.AddControllersWithViews();
 /*builder.Services.AddDbContext<Contexto> //Isa
     (options => options.UseSqlServer("Data Source=SB-1490635\\SQLSENAI;Initial Catalog = ProjetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));*/
 
-builder.Services.AddDbContext<Contexto> //Melina
-    (options => options.UseSqlServer("Data Source=SB-1490650\\SQLSENAI;Initial Catalog = ProjetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));
+/*.Services.AddDbContext<Contexto> //Melina
+    (options => options.UseSqlServer("Data Source=SB-1490650\\SQLSENAI;Initial Catalog = ProjetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));*/
 
 /*builder.Services.AddDbContext<Contexto> //Vitor
     (options => options.UseSqlServer("Data Source=SB-1490654\\SQLSENAI;Initial Catalog = ProjetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));*/
