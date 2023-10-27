8c468bb872bcfb073a9eb50efba20ac923fd9e50
Ana-Caroline-Gouvea <anagouvea.senai@gmail.com>
2023-10-27 11:51:30 -0300

    Adicionando as barras de pesquisa

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-/*builder.Services.AddDbContext<Contexto> //Carol
-    (options => options.UseSqlServer("Data Source=SB-1490638\\SQLSENAI;Initial Catalog = ProjetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));*/
+builder.Services.AddDbContext<Contexto> //Carol
+    (options => options.UseSqlServer("Data Source=SB-1490638\\SQLSENAI;Initial Catalog = ProjetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));
 
 /*builder.Services.AddDbContext<Contexto> //Isa
     (options => options.UseSqlServer("Data Source=SB-1490635\\SQLSENAI;Initial Catalog = ProjetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));*/
@@ -15,8 +15,8 @@ builder.Services.AddControllersWithViews();
 /*builder.Services.AddDbContext<Contexto> //Melina
     (options => options.UseSqlServer("Data Source=SB-1490650\\SQLSENAI;Initial Catalog = ProjetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));*/
 
-builder.Services.AddDbContext<Contexto> //Vitor
-    (options => options.UseSqlServer("Data Source=SB-1490654\\SQLSENAI;Initial Catalog = ProjetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));
+/*builder.Services.AddDbContext<Contexto> //Vitor
+    (options => options.UseSqlServer("Data Source=SB-1490654\\SQLSENAI;Initial Catalog = ProjetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));*/
 
 var app = builder.Build();
 
